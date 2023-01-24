a15fbaeb2682381538ec72a055844450026b19c4
Serhii_R <rsp89@i.ua>
2023-01-24 23:23:17 +0200

    Added: New practice > unique_in_order.py

---
+++ Practice/unique_in_order.py
@@ -0,0 +1,28 @@
+def unique_in_order(sequence) -> list:
+    """
+Function takes as argument a sequence and returns a list of items 
+without any elements with the same value next to each other 
+and preserving the original order of elements.
+
+For example:
+
+unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
+    :param sequence: Any type of sequence (str., list, etc.)
+    """
+    result = []
+    if len(sequence) != 0:
+        result.append(sequence[0])
+        for ind, element in enumerate(sequence):
+            if sequence[ind] == result[-1]:
+                continue
+            else:
+                result.append(element)
+    return result
+
+
+print(unique_in_order('AAAABBBCCDAABBB'))
+# print(unique_in_order([1, 2, 2, 3, 3]))
+# print(unique_in_order('ABBCcAD'))
+# print(unique_in_order(''))
