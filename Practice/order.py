f693d7ce9d5a1802526f4c5a5cb2f60cd9fe26d7
Serhii_R <rsp89@i.ua>
2023-01-25 22:37:52 +0200

    Added: New practice > order.py

---
+++ Practice/order.py
@@ -0,0 +1,13 @@
+def order(sentence):
+    new_sentence = ''
+    nums = []
+    for word in sentence.split():
+        for x in word:
+            if x.isdigit():
+                nums.append(x)
+    result = dict(zip(nums, sentence.split()))
+    new_sentence = ' '.join(result.get(k) for k in sorted(result))
+    return new_sentence
+
+
+print(order("is2 Thi1s T4est 3a"))
