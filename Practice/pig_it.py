353a73a2a10528a0eee6237608463d264d0ca02b
Serhii_R <rsp89@i.ua>
2023-01-25 23:22:35 +0200

    Added: New practice > pig_it.py

---
+++ Practice/pig_it.py
@@ -0,0 +1,20 @@
+def pig_it(text):
+    result = text.split()
+    for word in result:
+        if word[0].isalpha():
+            new_word = f"{word[1:] + word[0] + 'ay'}"
+            result.insert(result.index(word), new_word)
+            result.remove(word)
+        else:
+            continue
+    return ' '.join(result)
+
+
+print(pig_it('Pig latin is cool'))
+print(pig_it('Hello world !'))
+
+
+# alternative version
+# import re
+# def pig_it(text):
+#     return re.sub(r'(\w{1})(\w*)', r'\2\1ay', text)
