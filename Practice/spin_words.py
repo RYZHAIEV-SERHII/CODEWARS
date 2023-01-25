31c1404ba0dd856b288f846fd5f2b9bb9bd95fa3
Serhii_R <rsp89@i.ua>
2023-01-26 00:29:51 +0200

    Added: New practice > spin_words.py

---
+++ Practice/spin_words.py
@@ -0,0 +1,15 @@
+def spin_words(sentence):
+    new_sentence = sentence.split()
+    for word in new_sentence:
+        if len(word) > 4:
+            new_sentence.insert(new_sentence.index(word), word[::-1])
+            new_sentence.remove(word)
+    return ' '.join(new_sentence)
+
+
+print(spin_words("Hey fellow warriors"))
+
+
+# alternative solution
+# def spin_words(sentence):
+#     return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
