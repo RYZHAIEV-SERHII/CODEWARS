52eee5ef26ad7d103ea2a82e089229e3982e758a
Serhii_R <rsp89@i.ua>
2023-06-04 19:52:57 +0300

    Added: New practice > last_digit.py

---
+++ Practice/last_digit.py
@@ -0,0 +1,14 @@
+def last_digit(a, b):
+    if b == 0:
+        return 1
+    elif a == 0:
+        return 0
+    else:
+        a_last_digit = a % 10
+        b_last_digit = b % 4 + 4 if b % 4 == 0 else b % 4
+        return pow(a_last_digit, b_last_digit, 10)
+
+
+print(last_digit(9, 7))
+print(last_digit(2 ** 200, 2 ** 300))
+print(last_digit(0, 0))
